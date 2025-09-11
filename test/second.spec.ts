19811488555839856456f4f34bfe4ce06d9475fa
Otavio Lemos <otaviolemos@gmail.com>
2025-09-10 21:34:27 -0300

    test: fix name

--- test/second.spec.ts
+++ test/second.spec.ts
@@ -2,7 +2,7 @@ import { test, describe } from 'node:test';
 import assert from 'node:assert';
 
 describe('Basic Test Suite', () => {
-  test('should pass a simple assertion', () => {
+  test('should pass a second simple assertion', () => {
     const expected = 4;
     const actual = 2 + 2;
     
